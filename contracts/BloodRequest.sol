4698e6ca12bac769d5dc56ea3923aaf895c917d0
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-19 22:45:50 +0530

    create function submitBloodReq

--- contracts/BloodRequest.sol
+++ contracts/BloodRequest.sol
@@ -15,4 +15,29 @@ contract BloodReq {
         bool fulfilled;
     }
 
+    // mapping of requesters to their blood requests
+    mapping(address => BloodRequest) public bloodRequests;
+
+    function submitBloodReq(
+        string memory _pname, 
+        string memory _contactNum, 
+        string memory _district, 
+        string memory _province, 
+        string memory _donationCenter, 
+        string memory _bloodType
+    ) public {
+        
+        BloodRequest memory newRequest = BloodRequest({
+            requester: msg.sender,
+            pname: _pname,
+            contactNum: _contactNum,
+            district: _district,
+            province: _province,
+            donationCenter: _donationCenter,
+            bloodType: _bloodType,
+            timestamp: block.timestamp,
+            fulfilled: false
+        });
+        bloodRequests[msg.sender] = newRequest;
+    }
 }
