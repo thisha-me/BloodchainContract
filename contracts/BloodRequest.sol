4902e67d05ba31aa87d68f5eeeb47f1a922bec61
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-23 16:13:57 +0530

    fix error

--- contracts/BloodRequest.sol
+++ contracts/BloodRequest.sol
@@ -193,26 +193,26 @@ contract BloodReq {
             );
     }
 
-    function getUserDetailsById(address _user) public view returns (
-        string memory, 
-        string memory, 
-        string memory, 
-        uint256, 
-        string memory, 
-        uint256,
-        BloodRequest[] memory,
-        BloodRequest[] memory) {
-        return (
-            userDetails[_user].name, 
-            userDetails[_user].contactNum, 
-            userDetails[_user].email, 
-            userDetails[_user].age, 
-            userDetails[_user].bloodType, 
-            userDetails[_user].donationCount,
-            userDetails[_user].bloodRequestsHistory,
-            userDetails[_user].bloodDonationsHistory
-            );
-    }
+    // function getUserDetailsById(address _user) public view returns (
+    //     string memory, 
+    //     string memory, 
+    //     string memory, 
+    //     uint256, 
+    //     string memory, 
+    //     uint256,
+    //     BloodRequest[] memory,
+    //     BloodRequest[] memory) {
+    //     return (
+    //         userDetails[_user].name, 
+    //         userDetails[_user].contactNum, 
+    //         userDetails[_user].email, 
+    //         userDetails[_user].age, 
+    //         userDetails[_user].bloodType, 
+    //         userDetails[_user].donationCount,
+    //         userDetails[_user].bloodRequestsHistory,
+    //         userDetails[_user].bloodDonationsHistory
+    //         );
+    // }
 
     function getUsers() public view returns (address[] memory) {
         return users;
